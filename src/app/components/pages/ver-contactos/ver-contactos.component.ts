c31b20f6e527c25d489cbb0b210b7f6fcd30d327
tomasjpardo <tomasjpardo@yahoo.es>
2024-05-17 17:15:25 +0200

    cambios en ver contactos. Botones de Ordenar por nombre e ID

--- src/app/components/pages/ver-contactos/ver-contactos.component.ts
+++ src/app/components/pages/ver-contactos/ver-contactos.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderLayoutComponent } from '../../layout/header/header.component';
 import { FooterLayoutComponent } from '../../layout/footer/footer.component';
 import { ContactsService } from '../../../services/api-agenda/contacts.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ver-contactos',
@@ -13,10 +14,16 @@ import { ContactsService } from '../../../services/api-agenda/contacts.service';
 })
 export class VerContactosComponent implements OnInit {
   contacts: any[] = [];
+  sortOrder: string = 'id'; // Default sort order
 
-  constructor(private contactsService: ContactsService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contactsService: ContactsService
+  ) {}
 
   ngOnInit(): void {
+    this.sortOrder = this.route.snapshot.queryParamMap.get('sortOrder') || 'id';
     this.loadContacts();
   }
 
@@ -25,10 +32,30 @@ export class VerContactosComponent implements OnInit {
       next: (data) => {
         console.log('Contacts loaded successfully', data);
         this.contacts = data.data;
+        this.sortContacts();
       },
+      error: (error) => {
+        console.error('Error loading contacts', error);
+      }
     });
-    };
-    refreshContacts(): void {
-      this.loadContacts();
+  }
+
+  sortContacts(): void {
+    if (this.sortOrder === 'name') {
+      this.contacts.sort((a, b) => a.name.localeCompare(b.name));
+    } else {
+      this.contacts.sort((a, b) => a.id - b.id);
     }
+  }
+
+  setSortOrder(order: string): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { sortOrder: order },
+      queryParamsHandling: 'merge',
+    }).then(() => {
+      this.sortOrder = order;
+      this.loadContacts();
+    });
+  }
 }
